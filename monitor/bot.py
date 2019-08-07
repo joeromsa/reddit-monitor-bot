4631e690e3299265a3cd605d9cd2750f46ef3238
joeromsa <47951551+joeromsa@users.noreply.github.com>
2019-08-07 10:20:13 -0500

    added comments

--- monitor/bot.py
+++ monitor/bot.py
@@ -2,7 +2,7 @@ import praw
 import sqlite3
 from twilio.rest import Client
 
-#Get the data from the database
+#Gets the individual id of each post stored in the database and returns it as a list.
 def get_data_id():
         conn = sqlite3.connect('database.db')
 
@@ -25,7 +25,8 @@ def get_data_id():
 
         return ar
 
-#check if already in datbase
+#Checks if a post is already in the datbase by using the list returned from get_data_id. If it is it is removed from the local lists
+#of id, titles, and urls.
 def check(id):
         for items in get_data_id():
                 num = 0
@@ -37,7 +38,7 @@ def check(id):
                         continue
                 num += 1
 
-#add data to the database
+#Inserts data to the database
 def add_data():
         conn = sqlite3.connect('database.db')
 
@@ -52,7 +53,7 @@ def add_data():
 
         conn.close()
 
-#returns the title of all posts
+#Returns the title of all posts and prints them to the screen. Used for testing purposes. 
 def check_items():
         conn = sqlite3.connect('database.db')
 
@@ -64,7 +65,7 @@ def check_items():
 
         conn.close()
 
-#deletes everything in the database
+#Deletes everything in the database. Used for testing purposes.
 def delete_all():
         conn = sqlite3.connect('database.db')
 
@@ -75,7 +76,8 @@ def delete_all():
        
         conn.close()
 
-#deletes 2/3 of the database
+#Deletes 2/3 of the database by fetchin gthe first entry in the data base and the last, then subtracting to find the toatl number of
+#of entries. If the total is larger than 200 it is then multiplied by 2/3 and all entries with a rowid less than that is deleted.
 def delete_most():
         conn = sqlite3.connect('database.db')
 
@@ -103,7 +105,7 @@ def delete_most():
         else:
                 conn.close()
 
-#gets all the rowid from the database
+#Gets all the rowids from the database and prints them to the screen. Used for testing purposes.
 def get_all_data():
         conn = sqlite3.connect('database.db')
 
@@ -117,26 +119,26 @@ def get_all_data():
         conn.close()
 
         
-#log into reddit api. Bot credintials in praw.ini
+#Logs into reddit api. Bot credintials located in praw.ini
 reddit = praw.Reddit('bot1')
 
-#client call
+#Client call for twilio api, used to send text messages.
 client = Client("AC10a274c321bf8dc2858c68027b3c1089", "78707f12326e57eec6da92a1269beb9e")
 
-#subreddit for the bot
+#Subreddit that the bot will be pulling from.
 subreddit = reddit.subreddit('buildapcsales')
 
-#variables with what we are looking for, the size, and the email object
+#Variables with what we are looking for.
 word = "[monitor]"
 size = "34\""
 desc = "ultrawide"
 
-#lists to hold title of posts, url, and id
+#Lists to hold title of posts, url, and id.
 posts = []
 url = []
 post_id = []
 
-#looking at the new submissions
+#Loop to pull the new submissions, break into parts, and add to respective lists.
 for submission in subreddit.new():
     if word in submission.title.lower() and size in submission.title or desc in submission.title.lower():
         post_id.append(submission.id)
@@ -144,10 +146,10 @@ for submission in subreddit.new():
         url.append(submission.url)
 
 
-#check if posts are new     
+#Checks if posts are new.     
 check(post_id)
 
-#notify of the new posts
+#Constructs message and sends as text.
 num = 0
 for data in posts:
         msg = posts[num] + "\n\n" + url[num]
@@ -155,7 +157,7 @@ for data in posts:
         client.messages.create(to="+14028850351", from_="+15313018826", body=msg)
         num += 1
 
-#add new data to the database
+#Adds new data to the database and deletes data if full enough.
 add_data()
 delete_most()
 
